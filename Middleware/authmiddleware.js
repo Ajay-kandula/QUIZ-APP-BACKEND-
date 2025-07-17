ed93e68c247dc96aeddefc455443dfefb6b2d10e
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-17 09:42:34 +0530

    "update"

--- Middleware/authmiddleware.js
+++ Middleware/authmiddleware.js
@@ -1,17 +1,24 @@
-    const jwt=require('jsonwebtoken')
-    const JWT_SECRET = process.env.JWT_SECRET;
-    const authmiddleware= async(req,res,next)=>{
-        const authHeader= req.headers.authorization
-        if(!authHeader || !authHeader.startsWith('Bearer ')){
-            return res.status(400).json({message:'Acess denied'})
-        }
-        const token= authHeader.split(' ')[1];
-        try{
-            const decoded= jwt.verify(token,JWT_SECRET)
-            req.user={userId:decoded.userId}
-            next();
-        }catch(err){
-            res.status(500).json({error:err.message})
-        }
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const authmiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(400).json({ message: 'Access denied' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    console.log("Decoded token: ", decoded); 
+    if (!decoded.userId) {
+      return res.status(401).json({ message: "Unauthorized user not found in request" });
     }
-    module.exports=authmiddleware
+    req.user = { userId: decoded.userId };
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = authmiddleware;
