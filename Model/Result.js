cedde791ac4abb035ba33ba9d011ad04d475ead9
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-17 10:06:09 +0530

    "update"

--- Model/Result.js
+++ Model/Result.js
@@ -1,13 +1,15 @@
-const mongoose=require('mongoose')
-const answerschema=new mongoose.Schema({
-    questionid:{type:mongoose.Schema.Types.ObjectId ,required:true},
-    selectedanswer:{type:String,required:true},
-    isCorrect:{type:Boolean,required:true}
+const mongoose = require('mongoose');
+
+const answerschema = new mongoose.Schema({
+  questionid: { type: mongoose.Schema.Types.ObjectId, required: true },
+  selectedanswer: { type: String, required: true },
+  isCorrect: { type: Boolean, required: true }
 });
-const resultSchema=new mongoose.Schema({
-    user:{type:mongoose.Schema.Types.ObjectId ,ref:'User',required:true},
-    quiz:{type:mongoose.Schema.Types.ObjectId ,ref:"Quiz",required:true},
-    answers:[answerschema],
-    score:{type:Number,required:true}
-},{timestamps:true})
-module.exports=mongoose.model('Result',resultSchema)
+
+const resultSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  answers: [answerschema],
+  score: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Result', resultSchema);
