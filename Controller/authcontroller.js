05d1c3f6903491c74753421de1af712b8d9b519d
Ajay-k9999 <ajaykundula@sltechnocrats.net>
2025-07-25 14:38:55 +0530

    "update"

--- Controller/authcontroller.js
+++ Controller/authcontroller.js
@@ -17,7 +17,7 @@ const login=async (req,res)=>{
     const{email,password}=req.body;
     try{
         const user= await User.findOne({email});
-        if(!user) res.status(400).json({message:"Invalid details"});
+        if(!user) return res.status(400).json({message:"Invalid details"});
         const isMatch= await bcrypt.compare(password,user.password);
         if(!isMatch) return res.status(400).json({message:'Invalid passowrd'});
         const token= jwt.sign({userId:user._id}, process.env.JWT_SECRET,{
